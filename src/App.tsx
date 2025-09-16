a61d5cf97f6716b16b7e1d0d93b8c23e0782d6af
RgMellon <renan_gian@hotmail.com>
2025-09-16 07:07:45 -0300

    feat: add fonts theme and path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import {
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_700Bold,
+    HostGrotesk_600SemiBold,
+    useFonts,
+} from "@expo-google-fonts/host-grotesk";
+
 export function App() {
+    const [isFontsLoaded] = useFonts({
+        HostGrotesk_400Regular,
+        HostGrotesk_500Medium,
+        HostGrotesk_700Bold,
+        HostGrotesk_600SemiBold,
+    });
+
+    if (!isFontsLoaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
