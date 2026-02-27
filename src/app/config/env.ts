942f8359d6081c57d8a313d1781f9f649628cdf6
RgMellon <renan_gian@hotmail.com>
2026-02-27 07:19:27 -0300

    fix: usar expo-constants para env vars

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,10 +1,13 @@
+import Constants from 'expo-constants';
 import z from 'zod';
 
 const schema = z.object({
    EXPO_PUBLIC_API_URL: z.string(),
 });
 
-const parsedEnv = schema.parse(process.env);
+const parsedEnv = schema.parse({
+    EXPO_PUBLIC_API_URL: Constants.expoConfig?.extra?.EXPO_PUBLIC_API_URL,
+});
 
 export const env = {
     api: {
